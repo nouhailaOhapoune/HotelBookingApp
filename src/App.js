0e7de76aca2849c8d368c4996bb2bf45b6f3d693
unknown <mano497>
2023-02-15 20:52:41 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React, { useState } from 'react';
+import IdentifyMembre from './components/IdentifyPage/identifyMembre';
+
+function App() {  
+  return (
+
+      <IdentifyMembre />
+      
+  );
+}
+
+export default App;
---
+++ src/components/HomePage/AirbnbHome.js
@@ -0,0 +1,91 @@
+import  './AirbnbHome.css';
+import BookPage from '../BookingPage/Book';
+import { useState } from 'react';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, set } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCA3p9Y-7SrBVkGvHilH_1mi-1UeXoSpK4",
+  authDomain: "hotelbookingapp-d8e79.firebaseapp.com",
+  databaseURL: "https://hotelbookingapp-d8e79-default-rtdb.firebaseio.com",
+  projectId: "hotelbookingapp-d8e79",
+  storageBucket: "hotelbookingapp-d8e79.appspot.com",
+  messagingSenderId: "267655430696",
+  appId: "1:267655430696:web:505d45b996f11738daeb1d",
+  measurementId: "G-M3E3YHC011"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase();
+
+const Home = () => {
+    const [valid, setvalid] = useState(true)
+    const [hide, sethide] = useState(true)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const book_date = document.getElementById('book-date');
+        const book_time = document.getElementById('book-time');
+        
+        const leave_date = document.getElementById('leave-date');
+        const leave_time = document.getElementById('leave-time');
+
+        if(!book_date.value || !book_time.value || !leave_date.value || !leave_time.value){
+            alert('Please fill all the fields');
+        }else{
+            writeUserData();
+        function writeUserData() {
+            const db = getDatabase();
+            set(ref(db, 'data/'), {
+            bookingdate: book_date.value,
+            bookingtime: book_time.value,
+            leave_date: leave_date.value,
+            leave_time: leave_time.value,
+            });
+        }
+            alert('Your booking has been made');
+            const BookPageDiv = document.querySelector('.box');
+            const container = document.querySelector('.container');
+            setvalid(!valid + BookPageDiv.classList.add("show"));
+            sethide(!hide + container.classList.add("hide"));
+        }
+    }
+  return (
+      <>
+        <BookPage />
+        <div className="container">
+      <div className="content">
+        <div className="text">Book Now</div>
+        <div className="form2">
+          <div className="txt">Date & Time you would like to Stay</div>
+          <form action="" onSubmit={handleSubmit}>
+            <div className="inputData">
+              <input type="date" name="" id="book-date"/>
+            </div>
+            <div className="inputData">
+              <input type="time" name="" id="book-time" />
+            </div>
+            <div className="txt">Date & Time you would like to leave</div>
+            <div className="inputData">
+              <input type="date" name="" id="leave-date" />
+            </div>
+            <div className="inputData">
+              <input type="time" name="" id="leave-time" />
+            </div>
+            <div className="book">
+                <button type="submit">Book</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+      </>
+  );
+};
+export default Home;
